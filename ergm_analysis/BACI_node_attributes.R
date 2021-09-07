ad5597050f32a7a0269d45da459f104ece134ba3
peter-herman <peter.r.herman@gmail.com>
2021-09-07 08:56:12 -0400

    Removed some old unused functions in BACI_node_attributes.R

--- ergm_analysis/BACI_node_attributes.R
+++ ergm_analysis/BACI_node_attributes.R
@@ -1,94 +1,4 @@
 
-
-BACI.node.attribute.builder <- function(data, cepii.geo.path, cepii.grav.path, EEA.entry, year.used){ 
-# INPUTS: "data" is what is returned from BACI.Data.Prep.
-#         "cepii.geo.path" is the file name and/or path for the geo.cepii data
-#         "cepii.grav.path" is the file name and/or path for the geo.gravity data
-
-library(foreign)
-require(tidyr)
-countrylist <- as.data.frame(data$countries)
-countrylist <- rename(countrylist, c("data$countries"="iso3"))
-cepii.geo <- read.dta(cepii.geo.path)
-cepii.grav <- read.dta(cepii.grav.path)
-EEA.entry <- read.csv(EEA.entry.path, header = TRUE, sep = ',')
-
-geo.keep <- c("iso3", "continent", "lang20_1", "lang20_2", "lang20_3","lang20_4", "colonizer1", "colonizer2", "colonizer3", "colonizer4") #list of variables to keep
-cepii.geo <- unique(cepii.geo[geo.keep]) #DRops duplicates (some countries have multiple listings due to multiple major cities (e.g. New York City and Washington))
-country.atts <- merge(countrylist, cepii.geo, by.x = "iso3", by.y = "iso3", all.x=TRUE) #all.x = TRUE keeps unmatched countries from the countrylist.
-
-grav.keep <- c("iso3_o", "gdp_o", "gdpcap_o", "gatt_o") #List of grav variables to keep
-cepii.grav <- subset(cepii.grav, year == year.used) #keeps only the desired year.
-cepii.grav <- unique(cepii.grav[grav.keep]) 
-country.atts <- merge(country.atts, cepii.grav, by.x = "iso3", by.y = "iso3_o", all.x=TRUE)
-
-##GDP Measures
-country.atts <- country.atts[order(-country.atts$gdp_o),] #Sorts on GDP
-GDP.top20 <- head(country.atts, n = 20L) #Graps first observations
-GDP.top20 <- as.vector(GDP.top20["iso3"]) #converts to vector
-country.atts$gdp.top20 <- ifelse(country.atts$iso3 %in% as.character(GDP.top20[,1]) ,1,0) #generates variable = 1 if the country is in the list of top countries.
-GDP.top50 <- head(country.atts, n = 50L)
-GDP.top50 <- as.vector(GDP.top50["iso3"])
-country.atts$gdp.top50 <- ifelse(country.atts$iso3 %in% as.character(GDP.top50[,1]) ,1,0)
-
-
-## Languages
-country.atts$english <- ifelse(country.atts$lang20_1 == "English" |country.atts$lang20_2 == "English" | country.atts$lang20_3 == "English" | country.atts$lang20_4 == "English" ,1,0)
-country.atts$french <- ifelse(country.atts$lang20_1 == "French" |country.atts$lang20_2 == "French" | country.atts$lang20_3 == "French" | country.atts$lang20_4 == "French" ,1,0)
-country.atts$spanish <- ifelse(country.atts$lang20_1 == "Spanish" |country.atts$lang20_2 == "Spanish" | country.atts$lang20_3 == "Spanish" | country.atts$lang20_4 == "Spanish" ,1,0)
-
-#Colonizers
-country.atts$colGBR <- ifelse(country.atts$colonizer1 == "GBR" |country.atts$colonizer2 == "GBR" | country.atts$colonizer3 == "GBR" | country.atts$colonizer4 == "GBR" ,1,0)
-country.atts$colFRA <- ifelse(country.atts$colonizer1 == "FRA" |country.atts$colonizer2 == "FRA" | country.atts$colonizer3 == "FRA" | country.atts$colonizer4 == "FRA" ,1,0)
-country.atts$colESP <- ifelse(country.atts$colonizer1 == "ESP" |country.atts$colonizer2 == "ESP" | country.atts$colonizer3 == "ESP" | country.atts$colonizer4 == "ESP" ,1,0)
-
-#NAFTA
-if( year.used >= 1994){
-  nafta.members <- c("USA", "CAN", "MEX")
-  country.atts$nafta <- ifelse(country.atts$iso3 %in% nafta.members,1,0) 
-}
-
-#EEA 
-country.atts <- merge(country.atts, EEA.entry, by.x = "iso3", by.y = "X", all.x=TRUE)
-country.atts$eea <- ifelse(country.atts$EEA_entry<= year.used,1,0)
-country.atts$eea[is.na(country.atts$eea)] <- 0
-
-#Region
-country.atts$americas <- ifelse(country.atts$continent == "America",1,0)
-country.atts$europe <- ifelse(country.atts$continent == "Europe",1,0)
-country.atts$africa <- ifelse(country.atts$continent == "Africa",1,0)
-country.atts$asia <- ifelse(country.atts$continent == "Asia",1,0)
-country.atts$pacific <- ifelse(country.atts$continent == "Pacific",1,0)
-
-
-keep <- c("iso3", "continent","gatt_o", "gdp.top20", "gdp.top50", "english", "french", "spanish", "colGBR", "colFRA", "colESP", "nafta", "eea", "americas", "europe" , "africa" , "asia" , "pacific")
-#keep <- c("V1", "gatt_o", "gdp.top20", "gdp.top50", "english", "french", "spanish", "colGBR", "colFRA", "colESP", "nafta", "eea")
-country.atts <- subset(country.atts, select = keep)
-country.atts <- country.atts[order(country.atts$iso3),]
-return(country.atts)
-}
-
-
-network.attribute.appender <- function(sub.network, node.atts){
-  #sub.network %v% "iso3" <- as.vector(node.atts$iso3)
-  #sub.network %v% "continent" <- node.atts$continent
-  #sub.network %v% "gatt" <- node.atts$gatt_o
-  #sub.network %v% "gdp.top20" <- node.atts$gdp.top20
-  #sub.network %v% "gdp.top50" <- node.atts$gdp.top50
-  #sub.network %v% "english" <- node.atts$english
-  #sub.network %v% "french" <- node.atts$french
-  #sub.network %v% "spanish" <- node.atts$spanish
-  #sub.network %v% "colGBR" <- node.atts$colGBR
-  #sub.network %v% "colFRA" <- node.atts$colFRA
-  #sub.network %v% "colESP" <- node.atts$colESP
-  #sub.network %v% "nafta" <- node.atts$nafta
-  #sub.network %v% "eea" <- node.atts$eea
-  return(sub.network)
-}
-
-
-
-
 subnetwork.unvalued <- function(data, cepii.grav, linktype, yr) #creates an unvalued network
 {
   require(foreign)
@@ -143,18 +53,7 @@ node.attribute.adder <- function(trade.network, data, cepii.grav, year.used, att
   return(agg.network)                           
   }
   
-node.gdp.adder <- function(trade.network, data, year.used)
-  #Uses the world bank data for GDP
-{ gdp.data <- read.dta("P:\\Documents\\Working Papers\\Data Various\\World Bank GDPs.dta")
-  countrylist <- data.frame(data$countries)
-  keepers <- c("countrycode", "year", "GDP")
-  
-  gdp.data <- unique(subset(gdp.data, year == year.used))
-  node.att.mat <- merge(countrylist, gdp.data, by.x = c("data.countries"), by.y = c("countrycode"), all.x = TRUE)
-  #node.att.mat[is.na(node.att.mat)] <- 
-  agg.network %v% attrib <- node.att.mat[,3]                           
-  return(agg.network)                           
-} 
+
 
 identify.missingdata.countries <- function(data, cepii.grav, yr, varbls)
 {
